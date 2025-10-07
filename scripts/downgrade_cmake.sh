ae74a36d5cad22835bd01722e26cd7f65945b07b
Juna <46632782+JunaMeinhold@users.noreply.github.com>
2025-10-07 21:21:12 +0200

    fix: update downgrade_cmake script to manually install CMake 3.31.9 for macOS

--- scripts/downgrade_cmake.sh
+++ scripts/downgrade_cmake.sh
@@ -5,6 +5,10 @@ PLATFORM="${GHA_OS_NAME}"
 
 if [[ "$PLATFORM" == "macos" ]]; then
     brew uninstall cmake
-    brew install cmake@3.31.9
-    brew link --force --overwrite cmake@3.31.9
+    wget https://github.com/Kitware/CMake/releases/download/v3.31.9/cmake-3.31.9-macos-universal.tar.gz
+
+    tar -xzf cmake-3.31.9-macos-universal.tar.gz
+
+    sudo mv cmake-3.31.9-macos-universal /usr/local/cmake-3.31.9
+    echo "/usr/local/cmake-3.31.9/CMake.app/Contents/bin" >> $GITHUB_PATH
 fi
